271a6cdff9d389b509f8ae71ba15570cbc5ab407
Mouloud Atmani <M.atmani1@uni.brighton.ac.uk>
2024-04-21 14:58:47 +0100

    CSS changes

--- frontend/src/pages/item.js
+++ frontend/src/pages/item.js
@@ -135,7 +135,7 @@ export function Item() {
                 </div>
                 <div className="md:col-span-2 md:row-start-4 row-start-3 p-6">
                     <div>
-                        <h2 >
+                        <h2>
                             <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-gray-500 border-t-4 border-gray-200 rounded-t-xl gap-3" data-accordion-target="#accordion-open-body-1" aria-expanded="true" aria-controls="accordion-open-body-1">
                                 <span className="flex items-center"><svg class="w-5 h-5 me-2 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>Delivery Information</span>
                             </button>
